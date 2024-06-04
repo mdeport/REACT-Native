7ed1f9287d78a666a1503bc3fbc85fc554a2ac3c
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-04 09:55:39 +0200

    ajout du screen splash

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,7 +1,5 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 
-
-
 export default function HomeScreen() {
   
   return (
--- app/_layout.js
+++ app/_layout.js
@@ -1,34 +1,7 @@
 import { Stack } from "expo-router";
-import logo from "../assets/logoVies.png";
-import { Image, StyleSheet, View, Text } from "react-native";
-import { Asset } from "expo-asset";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 export default function RootLayout() {
-  const [isLogoLoaded, setIsLogoLoaded] = useState(false);
-
-  useEffect(() => {
-    loadLogo();
-  }, []);
-
-  const loadLogo = async () => {
-    try {
-      await Asset.fromModule(logo).downloadAsync();
-      setTimeout(() => setIsLogoLoaded(true), 1500);
-    } catch (err) {
-      console.warn(err);
-    }
-  };
-
-  if (!isLogoLoaded) {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.logo} source={logo} />
-        <Text> Vous allez être rediriger vers l'application </Text>
-        <Text>SAUVER DES VIES</Text>
-      </View>
-    );
-  }
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -36,14 +9,3 @@ export default function RootLayout() {
     </Stack>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  logo: {
-    width: 380,
-    height: 120,
-  },
-});
